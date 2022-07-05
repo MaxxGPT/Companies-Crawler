bcb9b94f1c5af0172c61f2469f54d1510fbe6054
Thomas Maxwell <maxxabillion@asatera.com>
2022-07-04 20:48:32 -0400

    added selenium

--- scraper.py
+++ scraper.py
@@ -1,23 +1,26 @@
-from urllib.request import Request
 from bs4 import BeautifulSoup
-from urllib.request import Request, urlopen
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-url = 'https://growjo.com/industry/Cannabis'
-request = Request(
-    url,
-    headers={'User-Agent': 'Mozilla/5.0'}
-)
-page = urlopen(request)
-page_content_bytes = page.read()
-page_html = page_content_bytes.decode("utf-8")
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("--headless")
 
-soup = BeautifulSoup(page_html, "html.parser")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+url= 'https://growjo.com/industry/Cannabis'
+driver.get(url)
+time.sleep(2)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
 company_rows = soup.find_all("table",{"class":"jss31"})[0].find_all("tbody")[0].find_all("tr")
 
 for company in company_rows:
-    company_data = company.find_all("td")
-    logo = company_data[1].find_all("div",{"class":"lazyload-wrapper"})[0].find_all("a")
-    name = company_data[1].text
+    comapny_data = company.find_all("td")
+    logo = logo = comapny_data[1].find_all("div",{"class":"lazyload-wrapper"})[0]
+    name = comapny_data[1].text
     print(logo)
-    break
+
